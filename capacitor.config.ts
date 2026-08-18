1a3b71802652d76eca01b595dafa72b2b1b296cf
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2026-08-18 14:21:36 -0300

    fix(capacitor): use Play Console app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jcmradiorock.app',
-  appName: 'JCMRadioRock',
+  appId: 'com.app.jcmradiorock',
+  appName: 'JCM Radio',
   webDir: 'dist'
 };
 
